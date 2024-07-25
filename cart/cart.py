d41367288332b2cd195903599eda570fe35eae3a
Salgado <Se.salgado#duocuc.cl>
2024-07-25 14:48:44 -0400

    Trabajo en los agregar, actualizar, eliminar, total en el carrito

--- cart/cart.py
+++ cart/cart.py
@@ -1,24 +1,60 @@
+from store.models import Product
 
 class Cart():
     def __init__(self, request):
         self.session = request.session
-        # Obtener la clave de sesión actual si existe
         cart = self.session.get('session_key')
-        
-        # Si el usuario es nuevo, ¡sin clave de sesión! ¡Crea uno!
         if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
-            
-            
-        # Asegúrese de que el carrito esté disponible en todas las páginas del sitio.
         self.cart = cart 
-        
-    def add(self, product):
+
+    def add(self, product, quantity):
         product_id = str(product.id)
-        # Logica
+        product_qty = str(quantity)
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
-            
+            self.cart[product_id] = int(product_qty)
         self.session.modified = True
+
+    def cart_total(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        quantities = self.cart
+        total = 0
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total += product.sale_price * value
+                    else:
+                        total += product.price * value
+        return total
+        
+        
+        
+    def __len__(self):
+        return len(self.cart)
+    
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
+    
+    def update(self, product_id, quantity):
+        product_id = str(product_id)
+        product_qty = int(quantity)
+        self.cart[product_id] = product_qty
+        self.session.modified = True
+        return self.cart
+    
+    def delete(self, product_id):
+        product_id = str(product_id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,32 +1,41 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import JsonResponse, HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 from store.models import Product
 from .cart import Cart
 
-# Create your views here.
-def cart_summary(request):
-    return render(request, "cart/cart_summary.html", {})
-
-
 def cart_add(request):
-    if request.method == 'POST':
-        product_id = request.POST.get('product_id')
-        if not product_id:
-            return HttpResponseBadRequest('No product_id provided')
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, quantity=product_qty)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'qty': cart_quantity})
+        return response
 
-        try:
-            product = Product.objects.get(id=product_id)
-        except Product.DoesNotExist:
-            return HttpResponseBadRequest('Product does not exist')
+def cart_summary(request):
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    quantities = cart.get_quants()
+    totals = cart.cart_total()
+    return render(request, "cart/cart_summary.html", {'cart_products': cart_products, 'quantities': quantities, 'totals': totals})
 
-        cart = Cart(request)
-        cart.add(product=product)
-
-        return JsonResponse({'Product Name': product.name})
-    return HttpResponseBadRequest('Invalid request method')
-    
 def cart_delete(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        cart.delete(product_id)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'qty': cart_quantity})
+        return response
     return render(request, "cart/cart_delete.html", {})
 
 def cart_update(request):
-    return render(request, "cart/cart_update.html", {})
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        cart.update(product_id=product_id, quantity=product_qty)
+        response = JsonResponse({'qty': product_qty})
+        return response
